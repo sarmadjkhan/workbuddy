5619948205ec36291df9e7500d566128d46d7ab6
Sarmad Junaid <sarmadjkhan@gmail.com>
2023-12-14 00:01:57 +0500

    Connected to the DB

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 
 const express = require("express")
+const mongoose = require("mongoose")
 const workoutRouter = require("./routes/workouts")
 
 config = process.env
@@ -17,10 +18,18 @@ app.use((req, res, next) => {
     next()
 })
 
+// Connect to DB
+mongoose.connect(config.MONGO_URI)
+    .then(() => {
+        // listen for requests
+        app.listen(config.PORT, () => {
+            console.log(`Connected to DB & Listening on port ${config.PORT}....`)
+        })
+    })
+    .catch((error) => {
+        console.log(error)
+    })
+
 // routes
 app.use('/api/workouts', workoutRouter)
 
-// listen for requests
-app.listen(config.PORT, () => {
-    console.log(`Listening on port ${config.PORT}....`)
-})
